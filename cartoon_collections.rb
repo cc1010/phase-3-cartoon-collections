9f70c328674b20dc5b743e635378b65432955c15
chaim <chaimyisroel@gmail.com>
2021-09-19 18:30:24 -0500

    commit tree dc956fcc8b8041135afe4ed04b26dace61950ee0

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves arr# code an argument here
   # Your code here
+  arr.each_with_index { |name,i| puts "#{i+1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet planeteer_calls # code an argument here
   # Your code here
+  planeteer_calls.map {|item| item.capitalize()+'!'}
 end
 
-def long_planeteer_calls# code an argument here
+assorted_words = ["two", "go", "industrious", "bop"]
+def long_planeteer_calls assorted_words # code an argument here
   # Your code here
+  assorted_words.any? {|item| item.length > 4}
 end
 
-def find_the_cheese# code an argument here
+cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese cheese_types# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find {|item |  item == "cheddar"||item ==  "gouda"||item ==  "camembert"  }
 end
+p find_the_cheese cheese_types
